d6fd011f645c8b3ff415f506f8f1aa98204ad8ab
Muhammad Taufiq Maulana <=>
2024-10-01 22:19:42 +0700

    Refactor MesinAbsensi model to include fillable fields Refactor route for /iclock/cdata endpoint Add route for /iclock/cdata endpoint Add .gitignore files for database, public, storage, bootstrap/cache, storage/app, storage/app/public, storage/framework/views, storage/framework/testing, storage/framework/cache, storage/framework/sessions, storage/framework/cache/data, storage/framework, .gitattributes, .gitignore, .editorconfig, app/Models/MesinAbsensi.php, tests/TestCase.php, .styleci.yml, tests/Unit/ExampleTest.php, database/seeders/DatabaseSeeder.php, app/Http/Middleware/EncryptCookies.php, tests/Feature/ExampleTest.php, app/Http/Middleware/VerifyCsrfToken.php, app/Http/Controllers/Controller.php, tests/CreatesApplication.php, app/Http/Middleware/TrustHosts.php, app/Http/Middleware/TrimStrings.php, app/Providers/BroadcastServiceProvider.php, package.json, app/Providers/AppServiceProvider.php, app/Http/Middleware/PreventRequestsDuringMaintenance.php Add getRequest method to ControllerIclock Add postCdata method to ControllerIclock Create AttendanceLog model Create Employee model Create FaceData model Create FpData model Create MesinCommand model Create employees table migration Create attendance_logs table migration Create mesin_commands table migration Create fp_datas table migration Create face_datas table migration Add iclock routes

--- app/Http/Controllers/ControllerIclock.php
+++ app/Http/Controllers/ControllerIclock.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
 use App\Models\MesinAbsensi;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ControllerIclock extends Controller
@@ -40,6 +43,85 @@ class ControllerIclock extends Controller
                 ]
             );
         }
+        return response('OK', 200, ['Content-Type' => 'text/plain']);
+    }
+
+    public function getRequest()
+    {
+        $mesin = MesinAbsensi::firstOrCreate(['sn' => request('SN')]);
+        $mesin->with([
+            'commands' => function ($query) {
+                $query->whereNull('completed_at');
+            }
+        ]);
+        $mesin->update(['last_sync' => now()]);
+        $commands = $mesin->commands;
+        if ($commands->isEmpty()) {
+            return response('OK', 200, ['Content-Type' => 'text/plain']);
+        }
+        $temp = "";
+        foreach ($commands as $command) {
+            $temp .= $command->command . "\n";
+            $command->update(['transmit_at' => now()]);
+        }
+        if ($temp == "") {
+            return response('OK', 200, ['Content-Type' => 'text/plain']);
+        }
+    }
+
+    public function postCdata()
+    {
+        if (!request()->has('SN')) {
+            return response('Error: sn is required', 400, ['Content-Type' => 'text/plain']);
+        }
+        if (!request()->has('table')) {
+            return response('Error: table is required', 400, ['Content-Type' => 'text/plain']);
+        }
+        $count = 0;
+        $sn = request('SN');
+        $mesin = MesinAbsensi::firstOrCreate(['sn' => $sn]);
+        $mesin->update(['last_sync' => now()]);
+        switch (request('table')) {
+            case 'ATTLOG':
+                $rows = explode("\n", request()->getContent());
+                $rows = array_filter($rows, function ($row) {
+                    return !empty($row);
+                });
+                $rows = array_map(function ($row) {
+                    return explode("\t", $row);
+                }, $rows);
+                $userPins = array_map(function ($row) {
+                    return $row[0];
+                }, $rows);
+                $users = Employee::whereIn('pin', $userPins)->get();
+                foreach ($rows as $row) {
+                    $user = $users->firstWhere('pin', $row[0]);
+                    if (empty($user)) {
+                        continue;
+                    }
+                    $temp_waktu = Carbon::parse($row[1]);
+
+                    $targetTime = $temp_waktu->copy()->setTime(12, 0);
+                    if ($targetTime->greaterThan($temp_waktu)) {
+                        $status = 'out';
+                        $row[2] = 1;
+                    } else {
+                        $status = 'in';
+                        $row[2] = 0;
+                    }
+
+                    $user->attendanceLogs()->create([
+                        'datetime' => $temp_waktu,
+                        'status' => $status,
+                    ]);
+                }
+                break;
+
+            default:
+                # code...
+                break;
+        }
+
         return response('OK', 200, ['Content-Type' => 'text/plain']);
     }
 }
